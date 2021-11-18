1204889ec650f84972e460dbff8f4dc001d80ea3
kosa_fruzsina <90498459+kosafruzsina@users.noreply.github.com>
2021-11-18 13:34:25 +0000

    Add files via upload

---
+++ drill8/swap.cpp
@@ -0,0 +1,48 @@
+#include "std_lib_facilities.h"
+void swap_v(int a,int b)
+{ 
+	int temp;
+	temp = a, a=b;
+	b=temp; 
+}
+
+void swap_r(int& a,int& b)
+{
+	int temp; 
+	temp = a, a=b; 
+	b=temp;
+}
+
+/*
+void swap_cr(const int& a, const int& b)
+{ 
+	int temp; 
+	temp = a, a=b;		
+	b=temp;
+
+	- const miatt nem valtozik az ertek -
+}
+*/
+
+int main()
+{
+	int x = 7 ;
+    int y = 9 ;
+
+	swap_v(x,y);
+ 	cout << "x = " << x << " y = " << y << endl;
+
+	swap_r(x,y);                              
+ 	cout << "x = " << x << " y = " << y << endl;
+
+   	swap_v(x,y);
+   	cout << "x = " << x << " y = " << y << endl; 
+   	
+	/*
+	swap_v(x,y);
+	cout << " x = " << x << " y = " << y << endl;
+	swap_v(7.7,9.9);
+	*/
+
+	return 0;
+}
